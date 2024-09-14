26940ee0a8f4a6bba9a995e8a3cff8012dd4958a
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-09-14 16:24:48 +0000

    understand the concept of const,let and var and make a table with the help of console.table

---
+++ lecture_4/let,const_and_var.js
@@ -0,0 +1,10 @@
+const accountId=52521
+let accountEmail="xyz@gmail.com"
+var accountPassword = "kduj@g52" // we should not prefer var because of scoping
+accountCity = "jaipur"
+// accountId=51256
+// console.log(accountId) we can not change accountId bacause of const keyword
+// accountEmail = "abc@gmail.com"
+// accountPassword = "india@+91"
+// accountCity = "Benguluru"
+console.table([accountId,accountEmail,accountPassword,accountCity])
