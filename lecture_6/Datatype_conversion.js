1a24bd6f6374be2302b96535aabe9dd91ee4ebfb
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-09-14 17:59:13 +0000

    Datatype_conversion

---
+++ lecture_6/Datatype_conversion.js
@@ -0,0 +1,27 @@
+let score = "divyansh"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1;
+// false =>0;
+
+let isLoggedIn = "divyansh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "divyansh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber) //33
+console.log(typeof stringNumber) //string
