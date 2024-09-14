7e971d0de9d9ba809543375e9249489110ad21d1
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-09-14 19:02:17 +0000

    string_to_number_convrsion

---
+++ lecture_6/string_to_number_conversion.js
@@ -0,0 +1,31 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber) //33
+console.log(typeof stringNumber) //string
+
+let value = 3
+let negValue = -value
+console.log(negValue) // -3
+
+//console.log(2+2); =>4
+//console.log(2-2); =>0
+//console.log(2*2); =>4
+//console.log(2**2); =>2 power 2=>4
+//console.log(2/2); =>1
+//console.log(2%2); =>0
+
+let str1 = "hello"
+let str2 = " divyansh"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2)  //12
+console.log(1 + "2")  //12
+console.log("1"+ 2 + 2); //122
+console.log(1 + 2 + "2"); 32
+
+console.log(+true); //1
+console.log(+""); //0
+//  console.log(true+) //it will give error
