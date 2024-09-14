351b055c751c70a069bc200fb188c3c0ff274c88
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-09-14 17:04:14 +0000

    understand the concept of datatypes_and_ECMA_standard.

---
+++ lecture_5/datatypes_and_ECMA_standard.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as new version
+
+//alert(3+3)//we are using nodejs;not browser
+console.log(3+
+    3)//code readability should be high
+    console.log("Divyansh sharma")
+
+    let name = "Divyansh"
+    let age = 18;
+    let isLoggedIn = false
+
+    // number => 2 to power 53
+    // bigInt
+    // string=> true/false
+    // null=> standalone value
+    // undefined =>
+        //object
+
+    console.log(typeof undefined);  //undefined
+    console.log(typeof null); // object
