f782841e390dd28cdd1712cece4cc3eeac763c45
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-09-15 06:39:46 +0000

    comparision of data types

---
+++ lecture_8/comparision_of_datatypes.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); //false(because first null converted into zero and then check condition )
+console.log(null == 0); // false (because null does not convert into zero because of equality)
+console.log(null >= 0); // true (because first null converted into zero and then check condition)
+
+console.log(undefined == 0); //false(because undefined does not convert into zero because of equality)
+console.log(undefined > 0); //false(because undefined does not convert into zero because of equality)
+console.log(undefined < 0); //false(because undefined does not convert into zero because of equality)
+
+// === 
+
+console.log("2" === 2); //false (it check datatype also)
