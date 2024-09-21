f42bd129218725b9093087f0ed03317814238ae2
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-09-21 06:41:51 +0000

    object de-structure and JSON

---
+++ lecture_18/object_de-structure_and_JSON.js
@@ -0,0 +1,25 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// console.log( course.courseInstructor) //hitesh
+
+// const {courseInstructor} = course
+// console.log(courseInstructor);//hitesh
+
+// const{courseInstructor:instructor}=course;
+// console.log(instructor); //hitesh
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",  //JSON format
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {}, //another format of JSON(javascript object notation)
+//     {}
+// ]
