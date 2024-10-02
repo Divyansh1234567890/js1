6b2ff4bfa89417cd3170a62ea9d553abdcba5452
Divyansh1234567890 <divyanshsharma151020@gmail.com>
2024-10-02 06:58:20 +0000

    for_in loop

---
+++ lecture_29/for_in.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);//we can not directly access values through
+//     // for in loop so we require myObject[key] to access keyvalues but we can directly access key.
+// }
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India") //we can not iterate over map through for in loop 
+
+// for (const key in map) {
+//     console.log(key);
+// }
